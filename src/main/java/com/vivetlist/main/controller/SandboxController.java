d403aedf631307a8f7b8907bd6c8552eb2d76980
Cooper Hanke <cooper.hanke@gmail.com>
2018-04-27 16:07:56 -0500

    made directory structure for java and made a sandbox file for templating to begin

---
+++ src/main/java/com/vivetlist/main/controller/SandboxController.java
@@ -0,0 +1,13 @@
+package com.vivetlist.main.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SandboxController {
+
+    @GetMapping("/sandbox")
+    public String standupSandbox() {
+        return "sandbox";
+    }
+}
