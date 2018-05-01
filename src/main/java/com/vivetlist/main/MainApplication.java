6e2f6ee4c213cc89d2b61b30185445f3430bca39
ssaegert <ssaegert@gmail.com>
2018-05-01 15:45:16 -0500

    Set up site for deployment

--- src/main/java/com/vivetlist/main/MainApplication.java
+++ src/main/java/com/vivetlist/main/MainApplication.java
@@ -2,11 +2,16 @@ package com.vivetlist.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MainApplication {
-
+public class MainApplication extends SpringBootServletInitializer {
 	public static void main(String[] args) {
 		SpringApplication.run(MainApplication.class, args);
+	}
+
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(MainApplication.class);
 	}
 }
