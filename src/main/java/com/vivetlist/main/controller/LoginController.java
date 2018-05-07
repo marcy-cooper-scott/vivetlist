754bede66d7c05d6346da43db040b7b1ae52e079
Cooper Hanke <cooper.hanke@gmail.com>
2018-05-07 11:59:42 -0500

    preparing to bring in other elements of page; in no particular order: meds, reminders, appts, calendar, json responders, openfda

--- src/main/java/com/vivetlist/main/controller/LoginController.java
+++ src/main/java/com/vivetlist/main/controller/LoginController.java
@@ -1,18 +1,19 @@
 package com.vivetlist.main.controller;
 
+import com.vivetlist.main.repos.MedicineRepo;
 import com.vivetlist.main.repos.UserRepo;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class LoginController {
     UserRepo userDao;
+    MedicineRepo medRepo;
 
-    public LoginController(UserRepo userDao){
+    public LoginController(UserRepo userDao, MedicineRepo medRepo){
         this.userDao = userDao;
+        this.medRepo = medRepo;
     }
 
     @GetMapping("/login")
@@ -20,7 +21,6 @@ public class LoginController {
 
     @PostMapping("/login")
     public String userLoggedIn(){
-
         return "redirect:/mylist";
     }
 
--- src/main/java/com/vivetlist/main/controller/ReminderController.java
+++ src/main/java/com/vivetlist/main/controller/ReminderController.java
@@ -1,6 +1,6 @@
 package com.vivetlist.main.controller;
 
-import com.vivetlist.main.models.Appointment;
+import com.vivetlist.main.models.Reminder;
 import com.vivetlist.main.models.User;
 import com.vivetlist.main.repos.ReminderRepo;
 import org.joda.time.DateTime;
@@ -23,13 +23,13 @@ public class ReminderController {
 
     @GetMapping("/reminders.json")
     @ResponseBody
-    public List<Appointment> reminderAsJSON() {
+    public List<Reminder> reminderAsJSON() {
         User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         return repo.findByUserID(user.getId());
     }
-
-    private Date convertDate(Date date) {
-        DateTime joda = new DateTime(date);
-        return joda.minusHours(5).toDate(); // get timezone offset here, fixes the issue
-    }
+//
+//    private Date convertDate(Date date) {
+//        DateTime joda = new DateTime(date);
+//        return joda.minusHours(5).toDate(); // get timezone offset here, fixes the issue
+//    }
 }
