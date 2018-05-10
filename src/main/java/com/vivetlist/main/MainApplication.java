a6a84cd98b6b8d9fba1ddbd364dcba17f611a22b
Cooper Hanke <cooper.hanke@gmail.com>
2018-05-10 00:48:37 -0500

    subsystem for scheduling, and error handling, has been started; can now send emails and integrating sms is next

--- src/main/java/com/vivetlist/main/MainApplication.java
+++ src/main/java/com/vivetlist/main/MainApplication.java
@@ -4,9 +4,19 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
 
 @SpringBootApplication
+@EnableScheduling
 public class MainApplication extends SpringBootServletInitializer {
+
+	@PostConstruct
+	void started() {
+		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(MainApplication.class, args);
 	}
