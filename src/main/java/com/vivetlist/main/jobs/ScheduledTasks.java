48731a8e65484b97b66968bd5a65422af05357ed
Cooper Hanke <cooper.hanke@gmail.com>
2018-05-10 01:50:45 -0500

    sends reminders once based on a set of rules, adding sms is the last thing

--- src/main/java/com/vivetlist/main/jobs/ScheduledTasks.java
+++ src/main/java/com/vivetlist/main/jobs/ScheduledTasks.java
@@ -10,8 +10,10 @@ import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashSet;
 import java.util.List;
 
 import org.joda.time.DateTime;
@@ -28,24 +30,17 @@ public class ScheduledTasks {
 
     private static final Logger log = LoggerFactory.getLogger(ScheduledTasks.class);
 
-    @Scheduled(fixedRate = 60000) // we will check every 60 seconds for updates
+    @Scheduled(fixedRate = 30000) // we will check every 30 seconds for updates
     public void fetchTimes() throws Exception {
         Iterable<Reminder> reminderList = reminderRepo.findAll();
-        ArrayList<Integer> doneList = new ArrayList<>();
         if (reminderList == null) {
             log.info("No reminders yet!");
         } else {
+            log.info("Running though the reminders...");
             for (Reminder reminder : reminderList) {
-                // straight away into the method, we will ignore any reminders that are done
                 Long id = reminder.getId(); //
                 Long type = reminder.getUnit().getId();
-                System.out.println(id.intValue());
-                if (doneList.contains(id.intValue())) {
-                    continue;
-                }
                 DateTime reminderTime = new DateTime(reminder.getScheduled_time()); // use JodaTime to get more time items
-                // for testing
-                // now for looking at the day of year for the database time
                 log.info("Getting reminder id: " + reminder.getId());
                 // let's find out what now is considered, and the parameters for determining when exactly to send things now
                 DateTime now = new DateTime();
@@ -54,21 +49,21 @@ public class ScheduledTasks {
                 int minuteOfHour = now.getMinuteOfHour();
                 // let's take the type of notification, and make it into an int to determine how to notify for the request
                 // logic: let's divide everything by the type of notification they want
-                if (type.intValue() == 1) {
+                if (type.intValue() == 2) {
                     // now, for an email, we will find out if it's the day, then send out the email
                     if (dayOfToday == reminderTime.getDayOfYear()) {
                         String email = reminder.getUser().getEmail();
                         service.sendEmail(email);
-                        doneList.add(id.intValue());
-                        log.info("Added " + id.intValue() + " to done list > EMAIL");
+                        reminderRepo.delete(reminder.getId());
+                        log.info("Deleted reminder " + id.intValue() + " from DB > email sent");
                     }
-                } else if (type.intValue() == 2) {
+                } else if (type.intValue() == 1) {
                     // now, this is a text, so we need to be more precise: day of year, hour of day, minute of hour
                     if (dayOfToday == reminderTime.getDayOfYear() && hourOfDay == reminderTime.getHourOfDay() && minuteOfHour == reminderTime.getMinuteOfHour()) {
                         String phone = reminder.getUser().getPhone_number();
                         log.error("Would have sent a text to: " + phone);
-                        doneList.add(id.intValue());
-                        log.info("Added " + id.intValue() + " to done list > SMS");
+                        reminderRepo.delete(reminder.getId());
+                        log.info("Deleted reminder " + id.intValue() + " from DB > sms sent");
                     }
                 } else if (type.intValue() == 3) {
                     if (dayOfToday == reminderTime.getDayOfYear() && hourOfDay == reminderTime.getHourOfDay() && minuteOfHour == reminderTime.getMinuteOfHour()) {
@@ -76,13 +71,13 @@ public class ScheduledTasks {
                         String phone = reminder.getUser().getPhone_number();
                         service.sendEmail(email);
                         log.error("Would have sent a text to: " + phone);
-                        doneList.add(id.intValue());
-                        log.info("Added " + id.intValue() + " to done list > EMAIL & SMS");
+                        reminderRepo.delete(reminder.getId());
+                        log.info("Deleted reminder " + id.intValue() + " from DB > email & sms sent");
                     }
                 }
-                    else {
-                        System.out.println("Something is wrong, this reminder has no id!");
-                    }
+                else {
+                    System.out.println("Something is wrong, this reminder has no id!");
+                }
             }
         }
     }
